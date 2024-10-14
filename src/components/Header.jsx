de4615670c93c5b3e8fd91511d79c32efca534dd
konstantinrts <k.developer@acceptance.ltd>
2024-10-15 01:10:19 +0300

    all files 19

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import React, { useEffect, useState } from "react";
 import "@/styles/header.scss";
 import "@/styles/base.scss";
 import Link from "next/link";
-import LogoWhite from "@/icons/other/LogoWhite";
-import { usePathname } from "next/navigation";
 import Menu from "@/icons/other/Menu";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const [submenuOpen, setSubmenuOpen] = useState({});
-  const pathname = usePathname();
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [buttonActive, setButtonActive] = useState(false);
 
   const menuOpen = () => {
     setMenuOpened(!menuOpened);
+    setButtonActive(!buttonActive);
     document.body.classList.toggle("no-scroll", !menuOpened);
   };
 
@@ -25,100 +25,109 @@ const Header = () => {
   };
 
   useEffect(() => {
-    setMenuOpened(false);
-    document.body.classList.remove("no-scroll");
-  }, [pathname]);
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 992);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <>
-      <header className="header">
-        <div className="header__container _container">
-          <div className="header__body">
-            <div className="header__col-01">
-              <Link href="/" className="header__logo">LOGO</Link>
-            </div>
-            <div className="header__col-02">
-              <button className="header__btn-menu"><Menu /></button>
-            </div>
+    <header className="header">
+      <div className="header__container _container">
+        <div className="header__body">
+          <div className="header__col-01">
+            <Link href="/" className="header__logo">LOGO</Link>
+          </div>
+          <div className="header__col-02">
+            <button onClick={() => menuOpen()} className={`header__menu-btn ${menuOpened ? "_active" : ""}`}>
+              {!menuOpened ? (
+                <img src="/images/menu-close.svg" alt="menu-burger" />
+              ) : (
+                <img alt="logo" src="/images/menu-open.svg" />
+              )}
+            </button>
+          </div>
 
-            {/* <div className={`header__menu ${menuOpened ? "_active" : ""}`}>
-              <nav className="header__nav">
-                <ul className="header__list">
-                  <li
-                    className={`header__item-menu _submenu ${submenuOpen["services"] ? "_open" : ""}`}
-                    onClick={(event) => toggleSubmenu("services", event)}
+          <div className={`header__menu ${menuOpened ? "_active" : ""}`}>
+            <nav className="header__nav _container">
+              <ul className="header__list">
+                <li
+                  className={`header__item-menu _submenu ${submenuOpen["services"] ? "_open" : ""}`}
+                  onClick={() => toggleSubmenu("services")}
+                >
+                  <div className="wrapper">
+                    <span>Services</span>
+                    <button type="button" className="btn">
+                      {!submenuOpen["services"] ? (
+                        <img src="/images/menu-right.svg" alt="expand" />
+                      ) : (
+                        <img src="/images/menu-left.svg" alt="collapse" />
+                      )}
+                    </button>
+                  </div>
+                  <ul
+                    className={`header__submenu ${submenuOpen["services"] ? "_active" : ""}`}
+                    style={{
+                      maxHeight: submenuOpen["services"] ? "1000px" : "0",
+                      overflow: "hidden",
+                      transition: "all 0.3s ease 0s",
+                      paddingTop: submenuOpen["services"] ? "10px" : "0",
+                    }}
                   >
-                    <div className="wrapper">
-                      <span>Services</span>
-                      <button type="button" className="btn">
-                        {!submenuOpen["services"] ? (
-                          <img src="/images/menu-plus.svg" alt="expand" />
-                        ) : (
-                          <img src="/images/menu-minus.svg" alt="collapse" />
-                        )}
-                      </button>
-                    </div>
-                    <ul
-                      className={`header__submenu ${submenuOpen["services"] ? "_active" : ""}`}
-                      style={{
-                        maxHeight: submenuOpen["services"] ? "200px" : "0",
-                        overflow: "hidden",
-                        transition: "all 0.3s ease 0s",
-                        borderTop: submenuOpen["services"] ? "1px solid #6d6d6d4d" : "none",
-                        paddingTop: submenuOpen["services"] ? "24px" : "0",
-                        marginTop: submenuOpen["services"] ? "24px" : "0",
-                      }}
-                    >
-                      <li className="header__subitem"><Link href="/it-consulting">IT Consulting</Link></li>
-                      <li className="header__subitem"><Link href="/marketing-consulting">Marketing Consulting</Link></li>
-                    </ul>
-                  </li>
+                    <li className="header__subitem"><Link href="/services/supplier-identification-and-connection">Supplier Identification and Connection</Link></li>
+                    <li className="header__subitem"><Link href="/services/marketing-personnel-and-employee-recruitment">Marketing Personnel and Employee Recruitment</Link></li>
+                    <li className="header__subitem"><Link href="/services/customer-introduction">Customer Introduction</Link></li>
+                    <li className="header__subitem"><Link href="/services/investment-opportunity-identification">Investment Opportunity Identification</Link></li>
+                    <li className="header__subitem"><Link href="/services/ongoing-strategic-networking-and-partnership-development">On-going Strategic Networking and Partnership Development</Link></li>
+                  </ul>
+                </li>
 
-                  <li className="header__item-menu"><Link href="/complex-solutions">Complex Solutions</Link></li>
-                  <li className="header__item-menu"><Link href="/market-research">Market Research</Link></li>
-
-                  <li
-                    className={`header__item-menu _submenu ${submenuOpen["company"] ? "_open" : ""}`}
-                    onClick={() => toggleSubmenu("company")}
+                <li
+                  className={`header__item-menu _submenu ${submenuOpen["company"] ? "_open" : ""}`}
+                  onClick={() => toggleSubmenu("company")}
+                >
+                  <div className="wrapper">
+                    <span>Areas of Expertise</span>
+                    <button type="button" className="btn">
+                      {!submenuOpen["company"] ? (
+                        <img src="/images/menu-right.svg" alt="expand" />
+                      ) : (
+                        <img src="/images/menu-left.svg" alt="collapse" />
+                      )}
+                    </button>
+                  </div>
+                  <ul
+                    className={`header__submenu ${submenuOpen["company"] ? "_active" : ""}`}
+                    style={{
+                      maxHeight: submenuOpen["company"] ? "200px" : "0",
+                      overflow: "hidden",
+                      transition: "all 0.3s ease 0s",
+                      paddingTop: submenuOpen["company"] ? "14px" : "0",
+                    }}
                   >
-                    <div className="wrapper">
-                      <span>Company</span>
-                      <button type="button" className="btn">
-                        {!submenuOpen["company"] ? (
-                          <img src="/images/menu-plus.svg" alt="expand" />
-                        ) : (
-                          <img src="/images/menu-minus.svg" alt="collapse" />
-                        )}
-                      </button>
-                    </div>
-                    <ul
-                      className={`header__submenu ${submenuOpen["company"] ? "_active" : ""}`}
-                      style={{
-                        maxHeight: submenuOpen["company"] ? "200px" : "0",
-                        overflow: "hidden",
-                        transition: "all 0.3s ease 0s",
-                        borderTop: submenuOpen["company"] ? "1px solid #6d6d6d4d" : "none",
-                        paddingTop: submenuOpen["company"] ? "24px" : "0",
-                        marginTop: submenuOpen["company"] ? "24px" : "0",
-                      }}
-                    >
-                      <li className="header__subitem"><Link href="/about">About</Link></li>
-                      <li className="header__subitem"><Link href="/people">People</Link></li>
-                      <li className="header__subitem"><Link href="/newsroom">Newsroom</Link></li>
-                    </ul>
-                  </li>
+                    <li className="header__subitem"><Link href="/technology-and-innovation">Technology and Innovation</Link></li>
+                    <li className="header__subitem"><Link href="/finance-and-investment">Finance and Investment</Link></li>
+                    <li className="header__subitem"><Link href="/marketing-and-professional-services">Marketing and Professional Services</Link></li>
+                    <li className="header__subitem"><Link href="/manufacturing-and-retail">Manufacturing and Retail</Link></li>
+                  </ul>
+                </li>
 
-                  <li className="header__item-menu"><Link href="/how-we-work">How we work</Link></li>
-                  <li className="header__item-menu"><Link href="/faq">FAQ</Link></li>
-                  <li className="header__item-menu"><Link href="/contacts">Contact</Link></li>
-                </ul>
-              </nav>
-            </div> */}
-
+                <li className="header__item-menu"><Link href="/insights">Insights</Link></li>
+                <li className="header__item-menu"><Link href="/about-us">About Us</Link></li>
+                <li className="header__item-menu"><Link href="/career">Career</Link></li>
+                <li className="header__item-menu"><Link href="/contacts">Contacts</Link></li>
+              </ul>
+            </nav>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
